ea5f72940ec83cada120c47f8e17fab76ea24a20
MarkSandoval22 <91386377+MarkSandoval22@users.noreply.github.com>
2023-11-04 18:54:38 -0600

    Initial commit

---
+++ Proyecto_Tienda_SpringBoot_Grupal/src/main/java/com/proyecto/tienda/ProyectoTiendaSpringBootGrupalApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTiendaSpringBootGrupalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTiendaSpringBootGrupalApplication.class, args);
+	}
+
+}
